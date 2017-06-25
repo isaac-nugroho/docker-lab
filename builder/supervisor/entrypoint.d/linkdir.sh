da809deedac8b93c2475f4af7aa0d44813125e44
Isaac Nugroho <inugroho@gmail.com>
2017-06-26 01:02:58 +0700

    add gradle workdir

--- builder/supervisor/entrypoint.d/linkdir.sh
+++ builder/supervisor/entrypoint.d/linkdir.sh
@@ -4,6 +4,15 @@ if [[ ! -d /data/repository ]]; then
   mkdir -p /data/repository
   chown -R nobody:nobody /data/repository
 fi
+if [[ ! -d /data/gradle-workdir ]]; then
+  mkdir -p /data/gradle-workdir
+  chown -R nobody:nobody /data/gradle-workdir
+fi
+
+cd /home/nobody
+ln -s /data/gradle-workdir .gradle
+chown nobody:nobody .gradle
+
 cd /home/nobody/.m2
 ln -s /data/repository repository
 chown nobody:nobody repository
