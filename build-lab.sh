0c2df80718ad7c72a9767557fdcc8f4fe8f65ddb
Isaac Nugroho <inugroho@gmail.com>
2017-07-02 16:52:57 +0700

    refactoring

--- build-lab.sh
+++ build-lab.sh
@@ -2,7 +2,9 @@
 PARENT=$(pwd)
 echo "Cleaning up ..."
 docker rmi inugroho/base-image
+docker rmi inugroho/base-image-jre
 docker rmi inugroho/phensley-dns
+docker rmi inugroho/activemq
 docker rmi inugroho/builder
 docker rmi inugroho/microservice-base
 
@@ -11,10 +13,20 @@ cd $PARENT/base-image
 docker build . -t inugroho/base-image
 docker push inugroho/base-image
 
+echo "base-image-jre"
+cd $PARENT/base-image-jre
+docker build . -t inugroho/base-image-jre
+docker push inugroho/base-image-jre
+
 echo "phensley-dns"
 cd $PARENT/phensley-dns
 docker build . -t inugroho/phensley-dns
 docker push inugroho/phensley-dns
+
+echo "activemq"
+cd $PARENT/activemq
+docker build . -t inugroho/activemq
+docker push inugroho/activemq
 
 echo "builder"
 cd $PARENT/builder
--- phensley-dns/run-dockerdns.sh
+++ phensley-dns/run-dockerdns.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export IP_ADDRESS="$(grep -m 1 "${HOSTNAME}" /etc/hosts | head -1 | awk '{IFS=" "; print $1 }')"
-mkdir -p /data/dns
+sleep 10
 echo "nameserver ${IP_ADDRESS}" > /data/dns/resolv.conf
 echo "search ${DOMAIN}" >> /data/dns/resolv.conf
-/dockerdns --domain ${DOMAIN:denise} --resolver $(grep -Eo "^nameserver\s+[0-9.]+" /etc/resolv.conf | awk '{ print $2; }')
+/home/nobody/dockerdns --domain ${DOMAIN:denise} --resolver $(grep -Eo "^nameserver\s+[0-9.]+" /etc/resolv.conf | awk '{ print $2; }')
