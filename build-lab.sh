59ea3783cecf5049eea3e02599f72e6e842a2dac
Isaac <isaac.nugroho@gmail.com>
2017-10-30 02:30:13 +0700

    A lot of updates

--- build-lab.sh
+++ build-lab.sh
@@ -4,43 +4,114 @@ echo "Cleaning up ..."
 
 echo "base-image"
 cd $PARENT/base-image
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
-
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 echo "base-consul"
 cd $PARENT/base-consul
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
-echo "base-image-jre"
-cd $PARENT/base-image-jre
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+#echo "base-image-jre"
+#cd $PARENT/base-image-jre
+#docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+#if [[ ! -z DO_PUSH ]]; then
+#  docker push inugroho/$(basename $(pwd))
+#fi
 
-echo "consul-jre"
-cd $PARENT/consul-jre
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+echo "base-oraclejdk8"
+cd $PARENT/base-oraclejdk8
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
+
+echo "base-oraclejre8"
+cd $PARENT/base-oraclejre8
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
+
+#echo "consul-jre"
+#cd $PARENT/consul-jre
+#docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+#if [[ ! -z DO_PUSH ]]; then
+#  docker push inugroho/$(basename $(pwd))
+#fi
 
 echo "consul-nomad"
 cd $PARENT/consul-nomad
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
-echo "base-consul-oraclejdk8"
+echo "consul-oraclejdk8"
 cd $PARENT/consul-oraclejdk8
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
+
+echo "consul-oraclejre8"
+cd $PARENT/consul-oraclejre8
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
+
+echo "consul-nomad-oraclejre8"
+cd $PARENT/consul-nomad-oraclejre8
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
 echo "consul-cassandra"
 cd $PARENT/consul-cassandra
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
 echo "consul-activemq"
 cd $PARENT/consul-activemq
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
 echo "microservice-base"
 cd $PARENT/microservice-base
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
-echo "builder"
-cd $PARENT/builder
-docker build . -t inugroho/$(basename $(pwd)) && docker push inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+#echo "builder"
+#cd $PARENT/builder
+#docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+#if [[ ! -z DO_PUSH ]]; then
+#  docker push inugroho/$(basename $(pwd))
+#fi
+
+echo "builder-oraclejdk8"
+cd $PARENT/builder-oraclejdk8
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
+
+echo "jenkins"
+cd $PARENT/jenkins
+docker build . -t inugroho/$(basename $(pwd)) && docker rmi $(docker images --filter "dangling=true" -q)
+if [[ ! -z DO_PUSH ]]; then
+  docker push inugroho/$(basename $(pwd))
+fi
 
 echo "Cleaning up ..."
 docker container prune --force
--- hashi-ui/run-hashi-ui.sh
+++ hashi-ui/run-hashi-ui.sh
@@ -1,4 +1,4 @@
 #!/bin/dumb-init /bin/sh
 set -e
 sleep 30
-hashi-ui --nomad-enable --consul-enable --nomad-address "http://$(hostname -i):4646"
+hashi-ui --nomad-enable --consul-enable --nomad-address $NOMAD_ADDRESS
